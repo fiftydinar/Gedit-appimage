3cfe67b6b9935e8e4d703fe91ceca9e2250b24a5
iVAN <88724353+ivan-hc@users.noreply.github.com>
2023-12-30 04:44:08 +0100

    Add gedit-plugins (no python support)

--- gedit-junest.sh
+++ gedit-junest.sh
@@ -3,7 +3,7 @@
 # NAME OF THE APP BY REPLACING "SAMPLE"
 APP=gedit
 BIN="$APP" #CHANGE THIS IF THE NAME OF THE BINARY IS DIFFERENT FROM "$APP" (for example, the binary of "obs-studio" is "obs")
-DEPENDENCES="gsettings-desktop-schemas gspell libgedit-amtk libpeas python-gobject tepl"
+DEPENDENCES="gsettings-desktop-schemas gspell libgedit-amtk libpeas python-gobject tepl gedit-plugins"
 #BASICSTUFF="binutils gzip"
 #COMPILERS="gcc"
 
